9bb57817539769e3a36c28ae4dedb87a215faa81
Vuyile Sixaba <ssamoraam@gmail.com>
2024-08-26 23:42:08 +0200

    Including whole data dictionary

--- model.py
+++ model.py
@@ -122,15 +122,24 @@ dim_kena__patient_visit_report contains fields such as:
 - ENDED_AT: Timestamp when the conversation concluded, either by clinician or system closure.
 - QUEUE_JOINED_AT: Timestamp marking when the patient joined the clinician queue.
 - QUEUE_ENDED_AT: Timestamp marking when the patient was picked from the clinician queue by a clinician.
+- QUEUE_DURATION: Duration of time the patient spent in the clinician queue.
+- QUEUE_DURATION_IN_SECONDS: Duration of time the patient spent in the clinician queue, in seconds.
+- CONSULT_DURATION: Total duration of time of the consultation.
+- CONSULT_DURATION_IN_SECONDS: Total duration of time of the consultation, in seconds.
 - SNOOZE_DURATION: Total time for all snoozes within a consultation.
+- CONSULT_DURATION_MINUS_SNOOZE: Total duration of the consultation after removing the snooze duration.
 - SNOOZE_COUNT: The total number of times a consultation was snoozed.
 - CLINICIAN_START_AT: Timestamp when the clinician picked the patient from the queue and initiated contact.
 - CLINICIAN_ENDED_AT: Timestamp when the clinician concluded the interaction with the patient.
+- CLINICIAN_DURATION: Duration of time of the row-specific clinician in a consultation.
+- AVERAGE_SNOOZE_PER_CLINICIAN: The average snooze duration per clinician in a consultation.
+- CLINICIAN_DURATION_MINUS_SNOOZE: Duration of time of the row-specific clinician after removing the average snooze duration per clinician.
+- CLINICIAN_DURATION_IN_SECONDS: Duration of time of the row-specific clinician in a consultation, in seconds.
 - STAFF_NAME: Name of the clinician who interacted with the patient.
 - STAFF_ROLE: The role of the corresponding clinician.
 - ASSIGNMENT_ORDER: The order in which the clinician saw the patient.
 - STAFF_VIEWS: The total number of clinicians assigned to the consultations.
-- CONVERSATION_TYPE: Mode of conversing in consultation.
+- CONVERSATION_TYPE: Mode of consultation.
 - CLINICIAN_ROLES_IN_CONSULT: The roles of all clinicians in the consultation.
 - CATEGORY: Symptom category tile selected at the start of the consultation.
 - RATED_AT: Timestamp for when the patient completed their rating on the Kena app.
@@ -140,6 +149,16 @@ dim_kena__patient_visit_report contains fields such as:
 - PATIENT_AGE: Age of the patient at the time of consultation.
 - AGE_CATEGORY: Age category the patient falls in.
 - PRIMARY_ICD10_CODE: The main symptom the patient is consulting for.
+- REFERRAL_LETTER: The referral letter sent to patients within the consultation.
+- REFERRAL_DOCUMENT_ISSUED: Whether or not a referral document was issued.
+- REFERRAL_CATEGORY: The category type of the referral.
+- REFERRAL_SECTOR: The sector to which the patient is referred.
+- REFERRAL_TYPE: The type of referral selected.
+- REFERRAL_SUBTYPE: The subtypes of the specified referral type.
+- SICK_NOTE_DOCUMENT_ISSUED: Whether or not a sick note document was issued.
+- SCRIPT_DOCUMENT_ISSUED: Whether or not a prescription note was issued.
+- ATTENDANCE_CERTIFICATE_DOCUMENT_ISSUED: Whether or not an attendance certificate was issued.
+- REGISTRATION_STATUS: Indicates whether the patient completed the registration process in full.
 - INVOICE_ID: Identifier for the consultation's invoice.
 - INVOICE_CREATED_AT: Timestamp when the invoice was generated.
 - AMOUNT: The total amount the consultation was invoiced for.
@@ -153,8 +172,12 @@ dim_kena__patient_visit_report contains fields such as:
 - PAYMENT_STATUS: The status of the transaction.
 - PROMOTION_CUSTOMER_NAME: The name of the promotion customer.
 - PROMOTION_NAME: The name of the promotion run by the partner.
+- PROMO_CODE: The promotion code provided to customers at consultation payment.
+- USER_ALLOCATED_VOUCHER_PROMO_CODE: The 16-digit voucher code allocated to a user for discount voucher redemption.
 - PROMOTION_ID: The unique promotion identifier.
 - CALL_IN_CONVERSATION: Whether a call occurred during the consultation.
+- PATIENT_CANCELLED: Whether the patient canceled the consultation.
+- CLOSE_REASON: Reason provided for the cancellation of the consultation.
 """
 
 # Chat UI
